e3c4c3196ad737130b56772b7e7b81a24a337788
Alexey <Alexey@LAPTOP-7447NJ2L>
2023-01-22 15:15:01 +0300

    Update tests

--- tests.py
+++ tests.py
@@ -13,6 +13,10 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(replace_content('<html><div>blacK ruSsia</div></html>'),
                          '<html><div>BlackHub Games</div></html>')
 
+    def test_ignoring_newline(self):
+        self.assertEqual(replace_content('<html><div>blacK \n ruSsia</div></html>'),
+                         '<html><div>BlackHub Games</div></html>')
+
     def test_dividing_by_one_tag(self):
         self.assertEqual(replace_content('<html><div>blacK <span> ruSsia</div></html>'),
                          '<html><div>BlackHub <span> Games</div></html>')
