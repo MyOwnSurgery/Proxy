7ab8c61385c9885c4b7e29b8530fbb26b4716707
Alexey <Alexey@LAPTOP-7447NJ2L>
2023-01-22 19:27:13 +0300

    Add exceptions hierarchy

--- core/utils/exceptions.py
+++ core/utils/exceptions.py
@@ -1,16 +1,18 @@
-class NoSuchBrowser(Exception):
+class InvalidInput(Exception):
+    """ Base class for input errors """
+
+
+class NoSuchBrowser(InvalidInput):
     """ Specified browser is not enlisted """
+
+
+class DifferentLengthsOfStrings(InvalidInput):
+    """ Different lengths of string to replace and string to replace with """
+
+
+class TooMuchWordsInStrings(InvalidInput):
+    """ More than 2 words in strings """
 
 
 class BrowserFault(Exception):
     """ Problem with connecting driver to browser """
-
-
-class DifferentLengthsOfStrings(Exception):
-    """ Different lengths of string to replace and string to replace with """
-
-
-class TooMuchWordsInStrings(Exception):
-    """ More than 2 words in strings """
-
-
